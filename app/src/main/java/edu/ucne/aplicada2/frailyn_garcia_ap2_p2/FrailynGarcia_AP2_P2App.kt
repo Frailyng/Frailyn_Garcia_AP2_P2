6f5a87cee717635d4a9058614be42f9143083d60
frailyng <frailindes@gmail.com>
2025-06-18 19:10:48 -0400

    FrailynGarcia_AP2_P2App Creado

---
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/FrailynGarcia_AP2_P2App.kt
@@ -0,0 +1,8 @@
+package edu.ucne.aplicada2.frailyn_garcia_ap2_p2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FrailynGarcia_AP2_P2App: Application() {
+}
