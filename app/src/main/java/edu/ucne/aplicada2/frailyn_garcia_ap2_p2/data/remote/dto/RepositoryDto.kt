bc6f56192f8d9692f02f320a79f4d58fd89a703a
frailyng <frailindes@gmail.com>
2025-06-18 19:22:19 -0400

    ReposirtyDto creado

---
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/data/remote/dto/RepositoryDto.kt
@@ -0,0 +1,10 @@
+package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.data.remote.dto
+
+import com.squareup.moshi.Json
+
+
+data class RepositoryDto(
+    @Json(name = "name") val name: String,
+    @Json(name = "description") val description: String?,
+    @Json(name = "html_url") val htmlUrl: String
+)
