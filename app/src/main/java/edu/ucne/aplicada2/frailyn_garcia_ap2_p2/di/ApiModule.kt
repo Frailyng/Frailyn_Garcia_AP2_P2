7d0ab38c4df8ae7885b775da25544a8827abaeb7
frailyng <frailindes@gmail.com>
2025-06-18 20:16:53 -0400

    ApiModule Creado

---
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/di/ApiModule.kt
@@ -0,0 +1,36 @@
+package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.aplicada2.frailyn_garcia_ap2_p2.data.remote.RegistroApi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+import kotlin.jvm.java
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ApiModule {
+    const val BASE_URL = "https://api.github.com/users/enelramon/repos"
+
+    @Provides
+    @Singleton
+    fun providesMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory()).add(DateAdapter())
+            .build()
+
+    @Provides
+    @Singleton
+    fun providesUsuariosApi(moshi: Moshi): RegistroApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+            .create(RegistroApi::class.java)
+    }
+}
