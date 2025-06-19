3b7fabd517eec14dd3cd15614ba9f7c5302b2820
frailyng <frailindes@gmail.com>
2025-06-18 20:24:08 -0400

    HomeScreen Creado

---
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/Home/HomeScreen.kt
@@ -0,0 +1,86 @@
+package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.presentation.Home
+
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import edu.ucne.aplicada2.frailyn_garcia_ap2_p2.presentation.navigation.Screen
+
+@Composable
+fun HomeScreen(
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterVertically)
+    ) {
+
+        Text(
+            text = "Bienvenido",
+            style = MaterialTheme.typography.h4,
+            modifier = Modifier.padding(bottom = 8.dp),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black
+        )
+
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth(0.85f)
+                .shadow(elevation = 4.dp, shape = RoundedCornerShape(12.dp))
+                .clip(RoundedCornerShape(12.dp)),
+            color = MaterialTheme.colors.surface
+        ) {
+            Column(
+                modifier = Modifier.padding(12.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                OutlinedButton(
+                    onClick = { navController.navigate(Screen.RegistroList) },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(52.dp)
+                        .border(
+                            width = 1.dp,
+                            color = Color.Gray,
+                            shape = RoundedCornerShape(12.dp)
+                        ),
+                    shape = RoundedCornerShape(12.dp),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        backgroundColor = Color(0xFFF5F5F5),
+                        contentColor = Color.DarkGray
+                    )
+                ) {
+                    Text(
+                        text = "Registro Compras",
+                        fontSize = 16.sp,
+                        color = Color.DarkGray
+                    )
+                }
+
+            }
+        }
+    }
+}
