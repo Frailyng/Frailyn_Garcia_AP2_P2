50be21d38d5e46f2da6fa8c893ab9db60deb9e33
frailyng <frailindes@gmail.com>
2025-06-18 20:11:01 -0400

    RemoteDataSource modificado

--- app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/data/remote/RemoteDataSource.kt
@@ -1,4 +1,18 @@
 package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.data.remote
 
-class RemoteDataSource {
+import edu.ucne.aplicada2.frailyn_garcia_ap2_p2.data.remote.dto.RepositoryDto
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val registroApi: RegistroApi
+){
+    suspend fun getRegistros()= registroApi.getRegistros()
+
+    suspend fun updateRegistro(repositoryDto: RepositoryDto)= registroApi.updateRegistro(repositoryDto)
+
+    suspend fun saveRegistro(repositoryDto: RepositoryDto)= registroApi.saveRegistro(repositoryDto)
+
+    suspend fun  deleteRegistro(id: Int)= registroApi.deleteRegistro(id)
+
+    suspend fun getRegistros(id: Int)= registroApi.getRegistros(id)
 }
