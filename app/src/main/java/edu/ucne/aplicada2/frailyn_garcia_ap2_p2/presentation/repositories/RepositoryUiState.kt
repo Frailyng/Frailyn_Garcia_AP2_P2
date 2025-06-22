f50df4be6c98c8a99bdc6b788acafbafef3ce7fc
frailyng <frailindes@gmail.com>
2025-06-21 20:53:43 -0400

    RepositoryUiState Modificado

--- app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/repositories/RepositoryUiState.kt
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/repositories/RepositoryUiState.kt
@@ -2,16 +2,12 @@ package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.presentation.repositories
 
 import edu.ucne.aplicada2.frailyn_garcia_ap2_p2.data.remote.dto.RepositoryDto
 
-data class RegistroUiState(
+data class RepositoryUiState(
     val name: String = "",
     val description: String = "",
     val html_url: String = "",
     val errorMessage: String? = null,
     val isLoading: Boolean = false,
-    val isSuccess: Boolean = false,
-    val errorDescripcion: String? = null,
-    val errorMonto: String? = null,
-    val successMessage: String? = null,
-    val registros: List<RepositoryDto> = emptyList()
+    val repository: List<RepositoryDto> = emptyList()
 
 )
