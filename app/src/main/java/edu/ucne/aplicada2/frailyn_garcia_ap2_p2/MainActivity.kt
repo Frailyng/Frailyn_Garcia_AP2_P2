ca83c4c6589f81f170ffd4895ceb7788b753cde6
frailyng <frailindes@gmail.com>
2025-06-21 21:07:07 -0400

    MainActivity Modificado

--- app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +12,30 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.aplicada2.frailyn_garcia_ap2_p2.presentation.navigation.HomeNavHost
 import edu.ucne.aplicada2.frailyn_garcia_ap2_p2.ui.theme.Frailyn_Garcia_AP2_P2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Frailyn_Garcia_AP2_P2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                val nav = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Box(
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    ) {
+                        HomeNavHost(nav)
+                    }
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Frailyn_Garcia_AP2_P2Theme {
-        Greeting("Android")
     }
 }
