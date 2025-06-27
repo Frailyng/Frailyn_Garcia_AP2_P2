4e194a43a1d2429c3197db6b194a62c0bbb7cdd6
frailyng <frailindes@gmail.com>
2025-06-27 16:23:38 -0400

    RepositoryEvent Modificado

--- app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/repositories/RepositoryEvent.kt
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/repositories/RepositoryEvent.kt
@@ -1,7 +1,12 @@
 package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.presentation.repositories
 
-interface RepositoryEvent {
+sealed interface RepositoryEvent {
     data class NameChange(val name: String): RepositoryEvent
     data class DescriptionChange(val description: String): RepositoryEvent
     data class Html_urlChange(val html_url: String): RepositoryEvent
+
+    data object PostRepository: RepositoryEvent
+    data object GetRepositories: RepositoryEvent
+    data object PutRepositories: RepositoryEvent
+    data object DeleteRepositories: RepositoryEvent
 }
