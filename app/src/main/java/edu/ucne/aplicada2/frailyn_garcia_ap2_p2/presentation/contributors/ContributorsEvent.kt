5e62bc26cebd0d6703ed2b3a4cba9708ab4f927e
frailyng <frailindes@gmail.com>
2025-07-09 19:12:43 -0400

    ContributorsEvent Creado

---
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/contributors/ContributorsEvent.kt
@@ -0,0 +1,11 @@
+package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.presentation.contributors
+
+sealed interface ContributorsEvent {
+    data class LoginChange(val login: String): ContributorsEvent
+    data class Node_IdChange(val node_id: String): ContributorsEvent
+    data class Html_urlChange(val html_url: String): ContributorsEvent
+    data class ContributionsChange(val contributions: Int): ContributorsEvent
+
+
+    data object GetContributors: ContributorsEvent
+}
