ffd20e859e130a47b9977e5dc869a4c57ec361a9
frailyng <frailindes@gmail.com>
2025-07-09 20:09:33 -0400

    Screen Modificado

--- app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/presentation/navigation/Screen.kt
@@ -2,16 +2,17 @@ package edu.ucne.aplicada2.frailyn_garcia_ap2_p2.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
+@Serializable
 sealed class Screen {
-
     @Serializable
     data object Home : Screen()
-    @Serializable
-    data object RepositoryList: Screen()
 
     @Serializable
-    data object ContributorsList: Screen()
+    data object RepositoryList : Screen()
+
+    @Serializable
+    data class ContributorsList(val repositoryName: String) : Screen()
+
     @Serializable
     data class Repository(val registroId: Int?) : Screen()
-
 }
