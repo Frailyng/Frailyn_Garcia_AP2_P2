74bb0f3dadeefc9a4e021acad8d9d78904b59180
frailyng <frailindes@gmail.com>
2025-07-09 20:40:47 -0400

    RepositoryRepository Modificado el metodo de Get Contributors

--- app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/data/repository/RepositoryRepository.kt
+++ app/src/main/java/edu/ucne/aplicada2/frailyn_garcia_ap2_p2/data/repository/RepositoryRepository.kt
@@ -21,11 +21,12 @@ class RepositoryRepository @Inject constructor(
             emit(Resource.Error("Error al obtener repositorios: ${e.message}"))
         }
     }
+
     suspend fun getContributors(repos: String): Flow<Resource<List<ContributorsDto>>> = flow {
         emit(Resource.Loading())
         try {
-            val repos = remoteDataSource.getContributors(repos)
-            emit(Resource.Success(repos))
+            val contributors = remoteDataSource.getContributors(repos)
+            emit(Resource.Success(contributors))
         } catch (e: Exception) {
             emit(Resource.Error("Error al obtener los contribuidores: ${e.message}"))
         }
